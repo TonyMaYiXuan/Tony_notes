a932b0b0af96f86ec3c7ded32f11960d330cf3bc
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-12 00:35:03 +0800

    Delete files
    Not needed.

--- stringencoders-master/bootstrap.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-autoreconf --install --force
-automake --add-missing >/dev/null 2>&1
--- stringencoders-master/indent.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-set -ex
-if [ -z "${INDENT}" ]; then
-  INDENT=clang-format
-fi
-${INDENT} --version
-find . -name '*.[ch]' | xargs ${INDENT} -i -style=WebKit
-git status --porcelain
-
