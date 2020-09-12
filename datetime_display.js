c616b9e19651d7b8916353e5bb27ce7f5f13612d
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-12 22:51:58 +0800

    Delete datetime_display.js

--- datetime_display.js
+++
@@ -1,7 +0,0 @@
-function addLeadingZero(x) {
-    return ((x < 10)? ("0" + x): x);
-}
-var today = new Date();
-document.getElementById("datetime").innerHTML = today.getFullYear() + "-" + addLeadingZero(today.getMonth() + 1) + "-" + addLeadingZero(today.getDate()) + " " + addLeadingZero(today.getHours()) + ":" + addLeadingZero(today.getMinutes()) + ":" + addLeadingZero(today.getSeconds());
-document.getElementById("datetime").style.color = "#50F0A0"; // NEED quotes here
-document.getElementById("datetime").style.face = "Consolas";
