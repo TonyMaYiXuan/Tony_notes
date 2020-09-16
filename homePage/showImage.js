47a4c5824cc7f3abfa70ff763751469cfcaa0d24
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-16 18:53:21 +0800

    Amend showImage.js

--- homePage/showImage.js
+++ homePage/showImage.js
@@ -1,4 +1,4 @@
 document.getElementById('images_Welcome_to_Mathematics').style.display = 'none';
-document.getElementById('image_Welcome_to').src = '../Data/Image/Welcome_to.png';
-document.getElementById('image_Mathematics').src = '../Data/Image/Mathematics.png';
+document.getElementById('image_Welcome_to').src = '../DATA/image/Welcome_to.png';
+document.getElementById('image_Mathematics').src = '../DATA/image/Mathematics.png';
 document.getElementById('images_Welcome_to_Mathematics').style.display = 'block';
