49e057b855da690aed8cf9157f56f86c2b9d7545
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-16 18:11:03 +0800

    Amend goHomePage.js

--- goHomePage.js
+++ goHomePage.js
@@ -4,7 +4,7 @@ function goHomePage(flag) { //1 for return home and 0 for leaving home page
         document.getElementById('title').innerHTML = '<h1>Tony\'s note</h1>';
         document.getElementById('menuTable').innerHTML = '<tr><td align=\'center\'><div id=\'menuTableHome\'>START</div></td></tr>';
         document.getElementById('menuTableHome').style.backgroundImage = datadict.dayNight? 'linear-gradient(to top left, #284838, #082230)': 'linear-gradient(to left, #F0BBFF, #FFAAD0)';
-        document.getElementById('homePage').src = 'homePage.html';
+        document.getElementById('homePage').src = 'homePage/homePage.html';
     } else {
         changePaper(dataDict.currentPaper.paperName);
     }
