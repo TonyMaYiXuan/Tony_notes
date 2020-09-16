ede85a5d90dfb9277baa379c420bc5aeeff46bf3
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-16 13:32:10 +0800

    Encapsulate dateTimeDisplay.js

---
+++ dateTimeDisplay.js
@@ -0,0 +1,9 @@
+function addLeadingZero(n) {  // n is a string
+    return ((n < 10)? ('0' + n): n);
+}
+function datetimeDisplay() {
+    var today = new Date();
+    document.getElementById('datetime').innerHTML = today.getFullYear() + '-' + addLeadingZero(today.getMonth() + 1) + '-' + addLeadingZero(today.getDate()) + ' ' + addLeadingZero(today.getHours()) + ':' + addLeadingZero(today.getMinutes()) + ':' + addLeadingZero(today.getSeconds());
+    document.getElementById('datetime').style.color = '#50F0A0';
+    document.getElementById('datetime').style.face = 'Consolas';
+}
