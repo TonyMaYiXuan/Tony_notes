40b244c7c78c8e23d4038e2829dcc20508309024
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-16 16:18:48 +0800

    Encapsulate homePage.js
    1 for return home and 0 for leaving home page.

---
+++ homePage.js
@@ -0,0 +1,13 @@
+function homePage(flag) { //1 for return home and 0 for leaving home page
+    hideAll();
+    if (flag) {
+        document.getElementById('title').innerHTML = '<h1>Tony\'s note</h1>';
+        document.getElementById('menuTable').innerHTML = '<tr><td align=\'center\'><div id=\'menuTableHome\'>START</div></td></tr>';
+        document.getElementById('menuTableHome').style.backgroundImage = datadict.dayNight? 'linear-gradient(to top left, #284838, #082230)': 'linear-gradient(to left, #F0BBFF, #FFAAD0)';
+        document.getElementById('homePage').src = 'homePage.html';
+    } else {
+        changePaper(dataDict.currentPaper.paperName);
+    }
+    document.getElementById('buttonReturnHome').display.style = flag? 'none': 'block';
+    document.getElementById(flag? 'homePage': 'display').display.style = 'block';
+}
