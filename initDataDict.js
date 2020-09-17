bf221b18ac3ac81e0c118dd490feb26723a076fa
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-17 09:18:04 +0800

    Update initDataDict.js

--- initDataDict.js
+++ initDataDict.js
@@ -1,6 +1,5 @@
 var dataDict = {
         dayNight: 1,
-        externalJS: ['dateTimeDisplay.js', 'dayNightMode.js', 'changePaper.js', 'goHomePage.js', 'initPage.js'],
         userAgent: navigator.userAgent || navigator.vendor || window.opera,
         isIOS: /iPad|iPhone|iPod/.test(dataDict.userAgent) && !window.MSStream,
         isAndroid: /android/i.test(this.userAgent)
