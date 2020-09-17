7d08d6e518afca4fd8b9807d68b0e374105e11c9
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-17 10:39:54 +0800

    Fix setMenuDimensions.js

--- dogBoneShape/setMenuDimensions.js
+++ dogBoneShape/setMenuDimensions.js
@@ -1,5 +1,5 @@
 for (var i = 0; i != 4; i++) {
-    document.getElementsByTagName('td')[i].offsetWidth = document.getElementsByName('dogBoneShapeMainDisplay')[0].clientWidth;
+    document.getElementsByTagName('td')[i].offsetWidth = document.getElementsByName('dogBoneShapeMain')[0].clientWidth;
     document.getElementsByTagName('td')[i].offsetHeight = document.getElementsByName('dogBoneShapeMenu')[0].clientWidth / 4;
     document.getElementsByTagName('td')[i].align = 'center';
 }
