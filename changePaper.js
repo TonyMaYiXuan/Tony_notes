28289943730bd5820a602196e392887df6e0fbfc
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-18 00:09:51 +0800

    Fix changePaper.js

--- changePaper.js
+++ changePaper.js
@@ -3,4 +3,5 @@ function changePaper(paper) {
     window.top.dataDict.currentPaper = {paperName: paper, newest: window.top.dataDict.papers[paper]['File name'][0]};
     document.getElementById('mainDisplay').src = window.top.dataDict.papers[window.top.dataDict.currentPaper.paperName]['File link'][0];
     document.getElementById('title').innerHTML = '<h1>' + window.top.dataDict.papers[paper].Name + ' <font color=' + comfortableColor + '><i><sub>(Tony\'s notes)</sub></i></font></h1>';
+    dayNightMode(window.top.dayNight);
 }
