c8c1f3888ef2c61c76a2f3fd712f05f20aef8f40
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-17 10:43:01 +0800

    Update setDimensions.js

--- dogBoneShape/setDimensions.js
+++ dogBoneShape/setDimensions.js
@@ -1,4 +1,4 @@
-var tmp = document.getElementsByName('dogBoneShapeMain')[0].clientWidth;
+var tmp = document.getElementById('dogBoneShapeMain').clientWidth;
 tmp = dataDict.isAndroid? tmp * 2: tmp;
 document.getElementsByName('dogBoneShapeMenuFrame')[0].offsetHeight = tmp;
 document.getElementsByName('dogBoneShapeMainDisplay')[0].offsetHeight = tmp;
