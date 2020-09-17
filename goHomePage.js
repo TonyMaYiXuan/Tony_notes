4269a6e554a0aa4eb3ff5e3750d92726d00180a4
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-18 00:14:10 +0800

    Update goHomePage.js

--- goHomePage.js
+++ goHomePage.js
@@ -1,13 +1,13 @@
 function goHomePage(flag) { //1 for return home and 0 for leaving home page
-    hideAll();
+    // TODO Hide something
     if (flag) {
         document.getElementById('title').innerHTML = '<h1>Tony\'s note</h1>';
         document.getElementById('menuTable').innerHTML = '<tr><td align=\'center\'><div id=\'menuTableHome\'>START</div></td></tr>';
-        document.getElementById('menuTableHome').style.backgroundImage = datadict.dayNight? 'linear-gradient(to top left, #284838, #082230)': 'linear-gradient(to left, #F0BBFF, #FFAAD0)';
+        document.getElementById('menuTableHome').style.backgroundImage = window.top.datadict.dayNight? 'linear-gradient(to top left, #284838, #082230)': 'linear-gradient(to left, #F0BBFF, #FFAAD0)';
         document.getElementById('homePage').src = 'homePage/homePage.html';
     } else {
-        changePaper(dataDict.currentPaper.paperName);
+        changePaper(window.top.dataDict.currentPaper.paperName);
     }
     document.getElementById('buttonReturnHome').display.style = flag? 'none': 'block';
-    document.getElementById(flag? 'homePage': 'display').display.style = 'block';
+    document.getElementById('main').display.style = 'block';
 }
