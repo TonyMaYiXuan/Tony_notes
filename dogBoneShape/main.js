d9fb5f04e25e0c208d3f0a5a751a68d35a045899
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-17 09:29:17 +0800

    Fix main.js

--- dogBoneShape/main.js
+++ dogBoneShape/main.js
@@ -1,6 +1,6 @@
 if (typeof dataDict == "undefined") { // dogBoneShape opened as website on its own
     var externalJS = ['../initDataDict.js'];
-    dataDict.externalJS.forEach(item => {
+    externalJS.forEach(item => {
         var js = document.createElement('script');
         document.body.appendChild(js);
         js.src='item';
