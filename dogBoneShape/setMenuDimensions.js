63a38334c550841089ca9b9eedca697602575461
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-17 10:45:55 +0800

    Update setMenuDimensions.js

--- dogBoneShape/setMenuDimensions.js
+++ dogBoneShape/setMenuDimensions.js
@@ -1,5 +1,5 @@
 for (var i = 0; i != 4; i++) {
-    document.getElementsByTagName('td')[i].offsetWidth = document.getElementById('dogBoneShapeMain').clientWidth;
-    document.getElementsByTagName('td')[i].offsetHeight = document.getElementById('dogBoneShapeMenu').clientWidth / 4;
+    document.getElementsByTagName('td')[i].offsetWidth = document.getElementById('dogBoneShapeMain').style.clientWidth;
+    document.getElementsByTagName('td')[i].offsetHeight = document.getElementById('dogBoneShapeMenu').style.clientWidth / 4;
     document.getElementsByTagName('td')[i].align = 'center';
 }
