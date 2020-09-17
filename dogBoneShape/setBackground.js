adc0f5b119d943a0e054daf1ccf2ee9a1ce18398
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-17 09:30:20 +0800

    Update setBackground.js

--- dogBoneShape/setBackground.js
+++ dogBoneShape/setBackground.js
@@ -1,5 +1,5 @@
 if (typeof dataDict == "undefined") {dataDict = {dayNight: 1}; }
-const bgLinearGradient_const = [[128, 240, 128], [136, 232, 120], [128, 224, 128], [120, 232, 136], [128, 240, 128]], dayNightVariation = 40, repeat = 3;
+const bgLinearGradient_const = [[120, 240, 120], [136, 232, 120], [128, 224, 128], [120, 232, 136], [120, 240, 120]], dayNightVariation = 40, repeat = 3;
 var bgLinearGradient = [bgLinearGradient_const[0]];
 for (var i = 0; i < repeat; i++) {bgLinearGradient.concat(bgLinearGradient_const.slice(1, bgLinearGradient_const.length)); }
 var bgImage = '', tmp = dataDict.dayNight? -dayNightVariation: dayNightVariation;
