9dce3d180ddcdb01b38157dcfc8a8a4557f8d85e
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-18 13:22:48 +0800

    Update changePaper.js
    Minimum requirement would be on the desktop site of Android.

--- changePaper.js
+++ changePaper.js
@@ -3,8 +3,8 @@ function changePaper(paper) {
     window.top.dataDict.currentPaper = {paperName: paper, newest: window.top.dataDict.papers[paper]['File name'][0]};
     document.getElementById('mainDisplay').src = window.top.dataDict.papers[window.top.dataDict.currentPaper.paperName]['File link'][0];
     var tmpString = window.top.dataDict.papers[paper].Name;
-    if (tmpString.length > 40) {
-        tmpString = tmpString.substr(0, 40);
+    if (tmpString.length > 41) {
+        tmpString = tmpString.substr(0, 41) + '...';
     }
     document.getElementById('title').innerHTML = '<h1>' + tmpString + ' <font color=' + comfortableColor + '><i><sub>(Tony\'s notes)</sub></i></font></h1>';
     dayNightMode(window.top.dataDict.dayNight); // update font color of 'title'
