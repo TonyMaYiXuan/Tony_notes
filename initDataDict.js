052923687926f6ea2f4108f3386c1a96f8a3ead4
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-18 13:52:35 +0800

    Testing initDataDict.js

--- initDataDict.js
+++ initDataDict.js
@@ -1,8 +1,9 @@
 if (typeof window.top.dataDict == 'undefined') {
     window.top.dataDict = {
+        bodyPadding: 20,
         dayNight: 1,
         userAgent: navigator.userAgent || navigator.vendor || window.opera,
-        //isIOS: /iPad|iPhone|iPod/.test(this.userAgent) && !window.MSStream,
+        isIOS: /iPad|iPhone|iPod/.test(this.userAgent) && !window.MSStream,
+        isAndroid: /android/i.test(userAgent)
     };
-    window.top.dataDict.isAndroid = /android/i.test(window.top.dataDict.userAgent)
 }
