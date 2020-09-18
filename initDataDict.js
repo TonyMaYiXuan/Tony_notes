e65537d4c7c4a07dd661a8d43d25e7dccf77bc5b
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-18 15:59:03 +0800

    Testing initDataDict.js

--- initDataDict.js
+++ initDataDict.js
@@ -3,7 +3,7 @@ if (typeof window.top.dataDict == 'undefined') {
         bodyPadding: 20,
         dayNight: 1,
         userAgent: navigator.userAgent || navigator.vendor || window.opera,
-        isIOS: /iPad|iPhone|iPod/i.test(this.userAgent) && !window.MSStream,
+        isIOS: /iPad|iPhone|iPod/.test(this.userAgent) && !window.MSStream,
         isAndroid: /android/i.test(this.userAgent)
     };
     alert('alive in initDataDict.js; window.top.dataDict.isAndroid' + window.top.dataDict.isAndroid + this.dataDict.isAndroid);
