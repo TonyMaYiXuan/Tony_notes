62bb6958b091acdb82c9ec497e04d6528c74eba2
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-18 13:41:40 +0800

    Update changePaper.js
    Though isAndroid is not yet working on my phone Chrome browser.

--- changePaper.js
+++ changePaper.js
@@ -3,7 +3,7 @@ function changePaper(paper) {
     window.top.dataDict.currentPaper = {paperName: paper, newest: window.top.dataDict.papers[paper]['File name'][0]};
     document.getElementById('mainDisplay').src = window.top.dataDict.papers[window.top.dataDict.currentPaper.paperName]['File link'][0];
     var tmpString = window.top.dataDict.papers[paper].Name;
-    if (tmpString.length > 41) {
+    if (tmpString.length > 71 - window.top.isAndroid * 30) {
         tmpString = tmpString.substr(0, 41) + '...';
     }
     document.getElementById('title').innerHTML = '<h1>' + tmpString + ' <font color=' + comfortableColor + '><i><sub>(Tony\'s notes)</sub></i></font></h1>';
