5ed5c1747a37074f8fedb79c2f3b767f5da9dd4d
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-09-24 19:03:27 +0800

    Fix initDataDict.js

--- initDataDict.js
+++ initDataDict.js
@@ -6,5 +6,5 @@ if (typeof window.top.dataDict == 'undefined') {
         isIOS: /iPad|iPhone|iPod/i.test(this.userAgent) && !window.MSStream,
         isAndroid: /Android/i.test(this.userAgent)
     };
-    alert('alive in initDataDict.js; userAgent: ' + window.top.dataDict.userAgent + '; /android/i.test(this.userAgent): ' + window.top.dataDict.isAndroid);
+    alert('alive in initDataDict.js; userAgent: ' + window.top.dataDict.userAgent + '; /android/i.test(this.userAgent): ' + window.top.dataDict.isAndroid;
 }
