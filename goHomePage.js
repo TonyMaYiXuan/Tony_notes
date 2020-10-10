e74229d6e67da5649e3e4693ca984eeb9137871d
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-10-10 20:06:05 +0800

    Update goHomePage.js
    Refer to commit b6bb642

--- goHomePage.js
+++ goHomePage.js
@@ -1,4 +1,4 @@
-function menuTableHomeClicked() {
+function menuTableStartClicked() {
     var menuTableInnerHTML = '';
     for (var i = 0; i != Object.keys(window.top.dataDict.papers).length; i++) {
         var paper = window.top.dataDict.papers[Object.keys(window.top.dataDict.papers)[i]];
@@ -16,8 +16,7 @@ function goHomePage(flag) { //1 for returning home and 0 for leaving home page
     ['buttonReturnHome', 'main'].forEach((item) => {document.getElementById(item).style.display = 'none'; });  // TODO Maybe hide more things
     if (flag) {
         document.getElementById('title').innerHTML = '<h1>Tony\'s note</h1>';
-        document.getElementById('menuTable').innerHTML = '<tr><td align=\'center\' cellspacing=0 cellpadding=0><div id=\'menuTableHome\' onclick=\'menuTableHomeClicked();\'>START</div></td></tr>';
-        document.getElementById('menuTableHome').style.backgroundImage = window.top.dataDict.dayNight? 'linear-gradient(to bottom, #284838, #082230)': 'linear-gradient(to left, #F0BBFF, #FFAAD0)';
+        document.getElementById('menuTable').innerHTML = '<tr><td align=\'center\'><div id=\'menuTableStart\' onclick=\'menuTableStartClicked();\'>START</div></td></tr>';  // menuTableStart used in dayNightMode.js
         document.getElementById('mainDisplay').src = 'homePage/index.html';
     } else {
         changePaper(window.top.dataDict.currentPaper.paperName);
