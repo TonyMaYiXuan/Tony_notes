c207cbedb3205e06c11c5ebba0f542e9625cfb88
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-10-10 19:56:45 +0800

    Fix goHomePage.js

--- goHomePage.js
+++ goHomePage.js
@@ -16,7 +16,7 @@ function goHomePage(flag) { //1 for returning home and 0 for leaving home page
     ['buttonReturnHome', 'main'].forEach((item) => {document.getElementById(item).style.display = 'none'; });  // TODO Maybe hide more things
     if (flag) {
         document.getElementById('title').innerHTML = '<h1>Tony\'s note</h1>';
-        document.getElementById('menuTable').innerHTML = '<tr><td align=\'center\'><div id=\'menuTableHome\' cellspacing=0 cellpadding=0 onclick=\'menuTableHomeClicked();\'>START</div></td></tr>';
+        document.getElementById('menuTable').innerHTML = '<tr><td align=\'center\' cellspacing=0 cellpadding=0><div id=\'menuTableHome\' onclick=\'menuTableHomeClicked();\'>START</div></td></tr>';
         document.getElementById('menuTableHome').style.backgroundImage = window.top.dataDict.dayNight? 'linear-gradient(to bottom, #284838, #082230)': 'linear-gradient(to left, #F0BBFF, #FFAAD0)';
         document.getElementById('mainDisplay').src = 'homePage/index.html';
     } else {
