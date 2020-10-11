c4bd3d2a6e45fecfab2b831d1c4ca2ce07026932
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-10-11 08:13:08 +0800

    Modify goHomePage.js

--- goHomePage.js
+++ goHomePage.js
@@ -16,11 +16,13 @@ function goHomePage(flag) { //1 for returning home and 0 for leaving home page
     ['buttonReturnHome', 'main'].forEach((item) => {document.getElementById(item).style.display = 'none'; });  // TODO Maybe hide more things
     if (flag) {
         document.getElementById('title').innerHTML = '<h1>Tony\'s note</h1>';
-        document.getElementById('menuTable').innerHTML = '<tr><td align=\'center\'><div id=\'menuTableStart\' onclick=\'menuTableStartClicked();\'>START</div></td></tr>';  // menuTableStart used in dayNightMode.js
+        document.getElementById('menuTable').innerHTML = '<tr><td align=\'center\'><div id=\'menuTableStart\'>START</div></td></tr>';  // menuTableStart used in dayNightMode.js
+        document.getElementById('menuTableStart').onclick = menuTableStartClicked;
         document.getElementById('mainDisplay').src = 'homePage/index.html';
     } else {
         changePaper(window.top.dataDict.currentPaper.paperName);
     }
+    dayNightMode(1);
     document.getElementById('buttonReturnHome').style.display = flag? 'none': 'block';
     document.getElementById('main').style.display = 'block';
 }
