fb01159b2875c5738c063796fc38edb4b70cce5f
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-10-11 09:25:43 +0800

    Fix goHomePage.js
    I don't really know what's happening (as well as commit e8e3459), but I suggest writing code clearer, it helps...

--- goHomePage.js
+++ goHomePage.js
@@ -1,11 +1,13 @@
+function menuTableStartClickedSub(i) {
+    var paper = window.top.dataDict.papers[Object.keys(window.top.dataDict.papers)[i]];
+    window.top.dataDict.currentPaper = {paperName: paper['Name'], newest: paper['File name'][0]};
+}
 function menuTableStartClicked() /* simply change menuTable content */ {
     document.getElementById('menuTable').style.display = 'none';
     var menuTableInnerHTML = '';
     for (var i = 0; i != Object.keys(window.top.dataDict.papers).length; i++) {
         var paper = window.top.dataDict.papers[Object.keys(window.top.dataDict.papers)[i]];
-        alert(paper['Name']);
-        alert(paper);
-        var tmp = 'align=\'center\'><a onclick=\'window.top.dataDict.currentPaper = {paperName: "' + paper['Name'] + '", newest: "' + paper['File name'][0] + '"};goHomePage(0);\'>' + paper['Name'] + '</a></td>';
+        var tmp = 'align=\'center\'><a onclick=\'menuTableStartClickedSub(' + i.toString() + ');goHomePage(0);\'>' + paper['Name'] + '</a></td>';
         menuTableInnerHTML += window.top.dataDict.isAndroid? ('<td ' + tmp) /* same as width of menuTable */: ('<tr><td width=\'90%\' ' + tmp + '</tr>');
     }
     if (window.top.dataDict.isAndroid) {
