2dca06acd76c9f3e3f869f620bce3c82ece21858
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-10-11 08:28:19 +0800

    Amend goHomePage.js

--- goHomePage.js
+++ goHomePage.js
@@ -3,7 +3,7 @@ function menuTableStartClicked() /* simply change menuTable content */ {
     var menuTableInnerHTML = '';
     for (var i = 0; i != Object.keys(window.top.dataDict.papers).length; i++) {
         var paper = window.top.dataDict.papers[Object.keys(window.top.dataDict.papers)[i]];
-        var tmp = '<td align=\'center\' onclick=\'window.top.dataDict.currentPaper = {paperName: \'' + paper['Name'] + '\', newest: \'' + paper['File name'][0] + '\'};goHomePage(0);\'>' + paper['File name'][0] + '</td>';
+        var tmp = '<td width=\'100%\' align=\'center\' onclick=\'window.top.dataDict.currentPaper = {paperName: \'' + paper['Name'] + '\', newest: \'' + paper['File name'][0] + '\'};goHomePage(0);\'>' + paper['File name'][0] + '</td>';
         if (!window.top.dataDict.isAndroid) {
             tmp = '<tr>' + tmp + '</tr>';
         }
