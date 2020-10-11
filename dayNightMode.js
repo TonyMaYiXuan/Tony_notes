3fcf3228d0ff02b7c577ce89b1668e8f2fffff80
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-10-11 14:12:44 +0800

    Update dayNightMode.js

--- dayNightMode.js
+++ dayNightMode.js
@@ -1,10 +1,10 @@
 function dayNightMode(flag) {
     var dnModeAttr = { // day night mode attributes
             0: {
-                bodyBgImage: 'linear-gradient(to right, #E8FFFF, #FFFFE8)', bodyColor: '#101010', bodyFontFace: 'Calibri', h1Color: '#E080B0', h1FontFace: 'Calibri Light', menuBgColor: '#00FFCC', topMargin: 5, leftRightMargin: 6
+                bodyBgImage: 'linear-gradient(to right, #E8FFFF, #FFFFE8)', bodyColor: '#101010', bodyFontFace: 'Calibri', h1Color: '#E080B0', h1FontFace: 'Calibri Light', menuBgColor: '#00FFCC', menuBgImage: 'linear-gradient(to left, #BBFFCC, #FFEED0)', topMargin: 5, leftRightMargin: 6
             },
             1: {
-                bodyBgImage: 'linear-gradient(to left, #080040, #200030)', bodyColor: '#A0B0C0', bodyFontFace: 'Calibri', h1Color: '#AAEEFF', h1FontFace: 'Calibri Light', menuBgColor: '#0080B0', topMargin: 4, leftRightMargin: 5
+                bodyBgImage: 'linear-gradient(to left, #080040, #200030)', bodyColor: '#A0B0C0', bodyFontFace: 'Calibri', h1Color: '#AAEEFF', h1FontFace: 'Calibri Light', menuBgColor: '#0080B0', menuBgImage: 'linear-gradient(to right, #0080A0, #112233)', topMargin: 5, leftRightMargin: 6
             }
         };
     document.body.style.backgroundImage = dnModeAttr[flag].bodyBgImage;
@@ -16,7 +16,7 @@ function dayNightMode(flag) {
     document.getElementsByTagName('h1')[0].style.color = dnModeAttr[flag].h1Color;
     document.getElementsByTagName('h1')[0].style.fontFamily = dnModeAttr[flag].h1FontFace;
     if (window.top.dataDict.inHomePage == 1) {
-        document.getElementById('menuTable').style.backgroundImage = (flag? 'linear-gradient(to right, #0080A0, #112233)': 'linear-gradient(to left, #BBFFCC, #FFEED0)'); // the parenthesis should be important
+        document.getElementById('menuTable').style.backgroundImage = dnModeAttr[flag].menuBgImage;
     }
     else {
         document.getElementById('menuTable').style.backgroundColor = dnModeAttr[flag].menuBgColor;
