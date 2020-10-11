70928f5ce0060ad04b1a5cedcf2f888a198e0249
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-10-11 09:52:30 +0800

    Modify goHomePage.js

--- goHomePage.js
+++ goHomePage.js
@@ -21,12 +21,14 @@ function goHomePage(flag) { //1 for returning home and 0 for leaving home page
     ['buttonReturnHome', 'main'].forEach((item) => {document.getElementById(item).style.display = 'none'; });  // TODO Maybe hide more things
     if (flag) {
         document.getElementById('title').innerHTML = '<h1>Tony\'s note</h1>';
-        document.getElementById('menuTable').innerHTML = '<tr><td align=\'center\'><div id=\'menuTableStart\'>START</div></td></tr>';  // menuTableStart used in dayNightMode.js
+        document.getElementById('menuTable').innerHTML = (window.top.dataDict.isAndroid? '<tr><td align=\'center\'>START</td></tr>': '<tr><td width=\'90%\' align=\'center\'>START</td></tr>');
+        
         document.getElementById('menuTableStart').onclick = menuTableStartClicked;
         document.getElementById('mainDisplay').src = 'homePage/index.html';
     } else {
         changePaper(window.top.dataDict.currentPaper.paperName);
     }
+    window.top.dataDict.inHomePage = parseInt(flag);
     dayNightMode(window.top.dataDict.dayNight);
     document.getElementById('buttonReturnHome').style.display = flag? 'none': 'block';
     document.getElementById('main').style.display = 'block';
