07b28c149afdb2164c703b0f0fd332eb8db28e67
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-10-12 11:09:17 +0800

    Update initDataDict.js

--- initDataDict.js
+++ initDataDict.js
@@ -2,9 +2,10 @@ if (typeof window.top.dataDict == 'undefined') {
     window.top.dataDict = {
         bodyPadding: 20,
         dayNight: 1,
+        inHomePage: false,
+        menuWidth: 250, //maybe only for Windows
         userAgent: navigator.userAgent || navigator.vendor || window.opera
     };
     window.top.dataDict.isIOS = /iPad|iPhone|iPod/i.test(window.top.dataDict.userAgent) && !window.MSStream;
     window.top.dataDict.isAndroid = /Android/.test(window.top.dataDict.userAgent);
-    window.top.dataDict.inHomePage = false;
 }
