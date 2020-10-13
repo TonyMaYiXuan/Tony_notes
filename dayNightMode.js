09169c61c5695c48f4c8a7426ef69cd80cb79d26
TonyMaYiXuan <70892086+TonyMaYiXuan@users.noreply.github.com>
2020-10-13 19:11:59 +0800

    Fix dayNightMode.js

--- dayNightMode.js
+++ dayNightMode.js
@@ -26,7 +26,7 @@ function dayNightMode(flag) {
     if ((document.getElementById('mainDisplay').src == 'https://tonymayixuan.github.io/Tony_notes/homePage/index.html') && window.top.dataDict.inDogBoneShape) {
         document.getElementById('mainDisplay').contentWindow.setDogBoneShapeBackground();  //notice that this function uses window.top.dataDict.dayNight, but not local variable 'flag'
     }
-    if ((typeof document.getElementById('mainDisplay').contentWindow.dataDict != 'undefined') && (document.getElementById('mainDisplay').contentWindow.dataDict.src == 'description/index.html')) {
+    if ((document.getElementById('mainDisplay').src.search('https://tonymayixuan.github.io/Tony_notes/') != -1) && (typeof document.getElementById('mainDisplay').contentWindow.dataDict != 'undefined') && (document.getElementById('mainDisplay').contentWindow.dataDict.src == 'description/index.html')) {
         document.getElementById('mainDisplay').contentWindow.descriptionChangeColor();
     }
 }
